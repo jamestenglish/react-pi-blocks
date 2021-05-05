ab50325b915b8cd6174f83439c6a0098d574a89b
James English <james.t.english@saic.com>
2021-05-05 13:44:15 +0000

    update socket connection

--- ui/src/socket.js
+++ ui/src/socket.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import socketio from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:8080';
+const ENDPOINT = `${window.location.hostname}:8080`;
 const socket = socketio.connect(ENDPOINT, { reconnect: true });
 const SocketContext = React.createContext();
 
