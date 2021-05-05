1c7a9ba6ffb1528cf667fb9eca78d3d49c4ad854
James English <james.t.english@saic.com>
2021-05-05 13:58:24 +0000

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -eux
 
+sudo cp /home/pi/Development/react-pi-blocks/scripts/service/react-pi-blocks.service /etc/systemd/system/react-pi-blocks.service
+
 cd /home/pi/Development/react-pi-blocks
 git pull --rebase
 yarn setup
 
+sudo systemctl daemon-reload
 sudo systemctl restart react-pi-blocks
