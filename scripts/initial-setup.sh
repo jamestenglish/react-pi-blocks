6f4c9311aba1f74e7ffcbf8e7684a5dbc2a79b99
James English <james.t.english@saic.com>
2021-05-10 18:16:06 +0000

    init script

--- scripts/initial-setup.sh
+++ scripts/initial-setup.sh
@@ -1,23 +1,30 @@
 #!/bin/bash -eux
 NODE_VERSION="14.16.1"
 WHAT_AM_I=$(uname -m)
-if [[ "${WHAT_AM_I}" == "armv6l" ]]; then
-    echo "This is a pi zero, installing node"
-    PREV_DIR=`pwd`
-    mkdir -p /tmp/react-pi-blocks-install
-    cd /tmp/react-pi-blocks-install
-    wget "https://unofficial-builds.nodejs.org/download/release/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-armv6l.tar.gz"
-    tar -xf "node-v${NODE_VERSION}-linux-armv6l.tar.gz"
-    sudo mv "node-v${NODE_VERSION}-linux-armv6l" /usr/local/node
-    cd /usr/bin
-    sudo ln -s /usr/local/node/bin/node node
-    sudo ln -s /usr/local/node/bin/npm npm
-    node -v  # Verifying that the Node.js install worked
-    npm -v   # Verifying that the npm install worked
-    cd "$PREV_DIR"
+DO_NODE_INSTALL=${DO_NODE_INSTALL:-true}
+if [[ "${DO_NODE_INSTALL}" == "true" ]]; then
+
+    if [[ "${WHAT_AM_I}" == "armv6l" ]]; then
+        echo "This is a pi zero, installing node"
+        PREV_DIR=`pwd`
+        mkdir -p /tmp/react-pi-blocks-install
+        cd /tmp/react-pi-blocks-install
+        wget "https://unofficial-builds.nodejs.org/download/release/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-armv6l.tar.gz"
+        tar -xf "node-v${NODE_VERSION}-linux-armv6l.tar.gz"
+        sudo mv "node-v${NODE_VERSION}-linux-armv6l" /usr/local/node
+        cd /usr/bin
+        sudo ln -s /usr/local/node/bin/node node
+        sudo ln -s /usr/local/node/bin/npm npm
+        node -v  # Verifying that the Node.js install worked
+        npm -v   # Verifying that the npm install worked
+        cd "$PREV_DIR"
+    else
+        echo "not a pi zero, please install node yourself"
+    fi
 else
-    echo "not a pi zero, please install node yourself"
+    echo "skipping node install"
 fi
+
 sudo npm install --global yarn   
 # sudo ln -s /usr/local/node/bin/yarn /usr/local/bin/yarn
 # sudo ln -s /usr/local/node/bin/yarnpkg /usr/local/bin/yarnpkg
